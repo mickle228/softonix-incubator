59e83ef2839425a4d8a7c1757035a4ed88268537
mickle228 <olehkachan05@gmail.com>
2024-09-12 15:14:18 +0300

    fix error handling

--- src/homework/homework-1-promises.js
+++ src/homework/homework-1-promises.js
@@ -4,26 +4,22 @@
 // Функція `arrayHandler` приймає аргументом значення з поточної ітерації масиву `array`
 
 export const arrayHandler = (num) => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
-      if (num % 2 === 0) {
-        reject(new Error(`Error with number: ${num}`))
-      } else {
-        console.log(`Resolved: ${num}`)
-        resolve(num)
-      }
+      console.log(`Resolved: ${num}`)
+      resolve(num)
     }, num * 1000)
   })
 }
 
 const array = [1, 2, 3, 4, 5]
 
-async function handleArray (array) {
+async function handleArray(array) {
   for (let i = 0; i < array.length; i++) {
     try {
       await arrayHandler(array[i])
     } catch (error) {
-      console.error(error)
+      console.error(`Error handling ${array[i]}: ${error}`)
     }
   }
   console.log('All promises done;)')
